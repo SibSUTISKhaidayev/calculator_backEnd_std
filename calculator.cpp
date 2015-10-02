39b155e0c15acbda013a767f33b3ea0e3749e756
Khaidayev Yroslav <khaidayev@gmail.com>
2015-10-02 13:16:36 +0600

    add functions for line operations & cos, sin, extent

--- calculator.cpp
+++ calculator.cpp
@@ -1,7 +1,14 @@
 #include "calculator.h"
 
-calculator::calculator()
-{
 
+template<typename T>
+T calculator::summa(const T a, const T b)
+{
+    return a + b;
 }
 
+template<typename T>
+T calculator::differens(T a, T b)
+{
+    return a - b;
+}
--- calculator.h
+++ calculator.h
@@ -1,18 +1,33 @@
 #ifndef CALCULATOR_H
 #define CALCULATOR_H
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
 
 
 class calculator
 {
-private:
-    int a_;
-    int b_;
-
-    static calculator *instance_;
-    calculator();
 public:
-    calculator instance();
+
+    template<typename T>
+    static T summa(const T a, const T b);
+    template<typename T>
+    static T differens(T a, T b);
+    template<typename T>
+    static T multiplication(T a, T b);
+    template<typename T>
+    static T segmentation(T a, T b);
+
+    static double degree(const double a, const double b);
+
+    static double sinus(const double a);
+    static double cosinus(const double a);
+
 
 };
+
+
 
 #endif // CALCULATOR_H
